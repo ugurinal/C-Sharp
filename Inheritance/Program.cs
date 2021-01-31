9a5340b22854c36629b5051526a8808da89b7eca
UGURINAL <ugurinal01@hotmail.com>
2021-02-01 02:59:01 +0300

    Inheritance
    1 - base

---
+++ Inheritance/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Employee
+    {
+        public string fullName;
+
+        public Employee()
+        {
+            fullName = "Unknown";
+            Console.WriteLine("Parameterless Employee constructor called!");
+        }
+
+        public Employee(string fullName)
+        {
+            this.fullName = fullName;
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        public float yearlySalary;
+
+        public FullTimeEmployee()
+        {
+            Console.WriteLine("Parameterless FullTimeEmployee constructor called!");
+        }
+
+        public FullTimeEmployee(float yearlySalary, string test) : base(test)
+        {
+            this.yearlySalary = yearlySalary;
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public double hourlyRate;
+    }
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            FullTimeEmployee fte = new FullTimeEmployee(120000f, "james");
+
+            FullTimeEmployee fte1 = new FullTimeEmployee();
+        }
+    }
+}
