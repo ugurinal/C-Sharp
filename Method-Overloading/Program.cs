06081349ac779ece3bf4d3d253d282b155dc24c3
UGURINAL <ugurinal01@hotmail.com>
2021-02-02 21:55:36 +0300

    Method Overloading
    +initialize

---
+++ Method-Overloading/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    public static class Operations
+    {
+        public static int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public static int Add(int first, int second, int third)
+        {
+            return first + second + third;
+        }
+
+        public static int Add(int first, int second, out int third)
+        {
+            third = first + second;
+            return first + second;
+        }
+
+        public static float Add(float first, float second)
+        {
+            Console.WriteLine("Not params");
+
+            return first + second;
+        }
+
+        public static float Add(float first, params float[] arr)
+        {
+            Console.WriteLine("Params");
+
+            return first;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Operations.Add(5f, 6f));
+        }
+    }
+}
