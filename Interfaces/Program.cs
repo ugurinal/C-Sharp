43c3d70ba3f4afa0b58fede2f3e80ecba2cf6482
UGURINAL <ugurinal01@hotmail.com>
2021-02-02 23:56:14 +0300

    Interfaces
    +init

---
+++ Interfaces/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Interfaces
+{
+    #region Customer
+
+    internal interface ICustomer
+    {
+        void Print();
+
+        int Add(int x1, int x2);
+    }
+
+    public class Customer : ICustomer
+    {
+        public int Add(int x1, int x2)
+        {
+            return x1 + x2;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Print method");
+        }
+
+        public void Print(string x1)
+        {
+            Console.WriteLine(x1);
+        }
+    }
+
+    #endregion Customer
+
+    public interface Interface1
+    {
+        void Print();
+    }
+
+    public interface Interface2
+    {
+        void Print();
+    }
+
+    public class TestClass : Interface1, Interface2
+    {
+        void Interface1.Print()
+        {
+            Console.WriteLine("Interface 1");
+        }
+
+        void Interface2.Print()
+        {
+            Console.WriteLine("Interface 2");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.Print();
+            c1.Print("test");
+
+            ICustomer c2 = new Customer();
+
+            c2.Print();
+            ((Customer)c2).Print("asd");
+
+            Console.WriteLine(c2 is Customer ? "Yes" : "No");
+            Console.WriteLine(c1.GetType() == typeof(Customer) ? "Yes" : "No");
+
+            TestClass t1 = new TestClass();
+
+            ((Interface1)t1).Print();
+            ((Interface2)t1).Print();
+
+            Console.WriteLine();
+
+            (t1 as Interface1).Print();
+            (t1 as Interface2).Print();
+        }
+    }
+}
