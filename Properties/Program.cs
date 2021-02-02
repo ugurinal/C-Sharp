16ef96b1dcf59566edad88a77302b9300d848ef6
UGURINAL <ugurinal01@hotmail.com>
2021-02-02 22:05:11 +0300

    Properties
    +init

---
+++ Properties/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Base
+    {
+        private float _mass;
+        public float Mass { get { return _mass; } set { _mass = value; } }
+
+        private float _speed;
+        public float Speed { get { return _speed; } }   // no setter
+
+        public Base(float mass, float speed)
+        {
+            _mass = mass;
+            _speed = speed;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Base var1 = new Base(20, 6);
+
+            Console.WriteLine("Mass = {0} - Speed = {1}", var1.Mass, var1.Speed);
+
+            var1.Mass = 5f;
+
+            Console.WriteLine("Mass = {0} - Speed = {1}", var1.Mass, var1.Speed);
+        }
+    }
+}
