d124da4202b8fe13594b7b45ca4ad91738f919a5
UGURINAL <ugurinal01@hotmail.com>
2021-03-02 02:26:38 +0300

    Delegates
    delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        private delegate void HelloDelegate(string message);
+
+        private static void Main(string[] args)
+        {
+            HelloDelegate del = new HelloDelegate(Hello);
+
+            del += Test;
+            del += Test;
+            del += Test;
+            del += Test;
+            del -= Hello;
+
+            del("Hello from delegate");
+        }
+
+        public static void Hello(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Test(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
